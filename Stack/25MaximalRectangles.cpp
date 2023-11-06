7281dc34dae83efe8f72e8d0f6a7acaa397e35ce
Tosmim <tosmimforidmehtab@gmail.com>
2023-11-06 11:28:27 +0530

    Don't know how to do this

---
+++ Stack/25MaximalRectangles.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n"
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+using namespace std;
+// Stack and DP might be used
+// ================= BRUTE ====================
+
+// ================= BETTER ====================
+int maximalRectangle(vector<vector<char>>& matrix) {
+
+}
+// ================= BEST ====================
+
+int32_t main() {
+	fast
+
+
+	cout << endl;
+	cin.get();
+}
